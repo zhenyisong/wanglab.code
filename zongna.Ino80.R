0447702b95e1dacbd12fdda0b486af2b160e84e4
zhenyisong <zhenyisong@gmail.com>
2017-09-12 13:31:33 +0800

    sample-correlation

--- zongna.Ino80.R
+++ zongna.Ino80.R
@@ -1,32 +1,35 @@
+# @author  Yisong Zhen
+# @since   2017-09-12
+# @update 
+# I re-live this script according to the request by zongna;
+# 
 # the raw data were generated from next-seq 500
 # SE or PE?
 
-library(gplots)
-library(xlsx)
-library(Rsubread)
-library(edgeR)
-library(limma)
-library(DESeq2)
-library(gplots)
-library(genefilter)
-library(RColorBrewer)
-library(org.Rn.eg.db)
-library(cluster)
-library(factoextra)
-library(clusterProfiler)
-library(pathview)
-library(sva)
+# nohup R CMD BATCH /home/zhenyisong/biodata/wanglab/wangcode/zongna.Ino80.R &
 
 
-setwd('/home/zhenyisong/biodata/wanglab/zongna/Ino80Data/rsubread')
-setwd("D:\\wangli_data\\Rdata")
+pkgs         <- c( 'tidyverse','Rsubread', 'edgeR', 'limma','DiagrammeR',
+                   'gplots', 'genefilter', 'DESeq2', 'RColorBrewer',
+                   'org.Rn.eg.db', 'cluster', 'factoextra','clusterProfiler',
+                   'grid', 'gridExtra',
+                   'pathview','sva')
+load.lib     <- lapply(pkgs, require, character.only = TRUE)
+
+rat.genome.path    <- file.path('/mnt/date/igenomes/Rattus_norvegicus/UCSC/rn6/Sequence/WholeGenomeFasta/genome.fa')
+rat.gtf            <- file.path('/mnt/date/igenomes/Rattus_norvegicus/UCSC/rn6/Annotation/Genes/genes.gtf')
+Rdata.output.dir   <- file.path('/home/zhenyisong/biodata/wanglab/wangdata/Rdata')
+rsubread.index.lib <- file.path('/mnt/date/igenomes/rsubread')
+
+"
+setwd(Rdata.output.dir)
 load('zonnagIno80.Rdata')
-save.image(file = 'zonnagIno80.Rdata')
-quit('no')
+"
 
-
-rat.genome_ref.path   <- '/mnt/date/igenomes/Rattus_norvegicus/UCSC/rn6/Sequence/WholeGenomeFasta/genome.fa'
-rat.gtf               <- '/mnt/date/igenomes/Rattus_norvegicus/UCSC/rn6/Annotation/Genes/genes.gtf'
+"
+setwd('D:\\wangli_data\\Rdata')
+load('zonnagIno80.Rdata')
+"
 
 #---test code
 # for sva analysis
@@ -48,24 +51,30 @@ dir.create('rsubread')
 output.path          <- '/home/zhenyisong/biodata/wanglab/zongna/Ino80Data/rsubread'
 setwd('/home/zhenyisong/biodata/wanglab/zongna/Ino80Data/rsubread')
 rat.outputs.files    <- paste0(output.path,'/', reads.files.names,'.bam')
-rat.base.string      <- 'rn6_index'
+rat.base.string      <- 'rn6'
 
-buildindex( basename = rat.base.string, reference = rat.genome_ref.path )
+
+setwd(rsubread.index.lib)
 align( index         = rat.base.string, 
        readfile1     = rat.reads.paths, 
-       input_format  = "FASTQ", 
+       input_format  = 'FASTQ', 
        type          = 'rna',
        output_file   = rat.outputs.files, 
-       output_format = "BAM", 
-       nthreads      = 15, 
+       output_format = 'BAM', 
+       nthreads      = 6, 
        indels        = 1,
        maxMismatches = 3,
        phredOffset   = 33,
        unique        = T )
 
-gene.rat           <- featureCounts( rat.outputs.files, useMetaFeatures = TRUE, 
+zongna.Ino80.rat    <- featureCounts( rat.outputs.files, useMetaFeatures = TRUE, 
                                       annot.ext  = rat.gtf, isGTFAnnotationFile = TRUE,
-                                      nthreads   = 15, allowMultiOverlap = TRUE)
+                                      nthreads   = 6, allowMultiOverlap = TRUE)
+
+
+setwd(Rdata.output.dir)
+save.image(file = 'zonnagIno80.Rdata')
+quit('no')
 
 gene         <- gene.rat
 gene.counts  <- gene$counts
@@ -95,28 +104,42 @@ write.xlsx(rpkm.genes, file = 'zongna.lfc.xlsx',sheetName = 'log_transf_data', a
 # PCA
 #---
 
-dge.tmm    <- t(t(gene.exprs$counts) * gene.exprs$samples$norm.factors)
-dge.tmm.counts           <- apply(dge.tmm,2, as.integer)
 sample.info              <- data.frame( treat  = c('Control','Control',
                                                    'Treat','Treat') )
-dds                      <- DESeqDataSetFromMatrix( countData = dge.tmm.counts,
+zongna.vsd               <- zongna.Ino80.rat %$% counts %>%
+                            DESeqDataSetFromMatrix( countData = .,
                                                     colData   = sample.info,
-                                                    design    = ~ treat)
-vsd                      <- varianceStabilizingTransformation(dds);
-vsd.expr                 <- assay(vsd)
-#rownames(vsd.expr)       <- gene.exprs$genes$SYMBOL
-rownames(vsd.expr)       <- NULL
-colnames(vsd.expr)       <- c('Control-1','Control-2',
-                              'Ino80-1','Ino80-2')  
-sds <- rowSds(vsd.expr)
+                                                    design    = ~ treat) %>%
+                            DESeq() %>% getVarianceStabilizedData()
+                          
+rownames(zongna.vsd)       <- NULL
+colnames(zongna.vsd)       <- c('Control-1','Control-2',
+                                'Ino80-1','Ino80-2')  
+sds <- rowSds(zongna.vsd)
 sh  <- shorth(sds)
-vsd.filtered.expr <- vsd.expr[sds > 0.3,]
-my.palette        <- rev(colorRampPalette(brewer.pal(10, "RdBu"))(256))
-heatmap( cor(vsd.filtered.expr, method = 'spearman'), 
+vsd.filtered.exprs <- zongna.vsd[sds > 0.06,]
+my.palette         <- rev(colorRampPalette(brewer.pal(10, 'RdBu'))(10))
+
+zongna.table <- grid.newpage() %>% {cor(vsd.filtered.exprs)} %>%
+                tableGrob(rows = colnames(vsd.filtered.exprs)) %>%
+                grid.draw()
+
+pheatmap( cor(vsd.filtered.exprs), cluster_rows = T, cluster_cols = T,
+          color = my.palette, scale = 'none', fontsize_col = 8, 
+          fontsize_row = 8, cellwidth = 30, cellheight = 30,
+          labels_row = colnames(vsd.filtered.exprs), 
+          labels_col = colnames(vsd.filtered.exprs),
+          display_numbers = FALSE, number_color = 'orange',
+          fontsize_number = 10)
+
+
+"
+heatmap( cor(vsd.filtered.exprs, method = 'spearman'), 
          cexCol = 0.8, cexRow = 0.8, col = my.palette)
-pr <- prcomp(t(vsd.expr))
-plot(pr$x[,1:2], col = 'white', main = 'Sample PCA plot', type = "p")
-text(pr$x[,1], pr$x[,2], labels = colnames(vsd.expr), cex = 0.7)
+"
+pr <- prcomp(t(zongna.vsd))
+plot(pr$x[,1:2], col = 'white', main = 'Sample PCA plot', type = 'p')
+text(pr$x[,1], pr$x[,2], labels = colnames(zongna.vsd), cex = 0.7)
 pr.var <- pr$sdev^2
 pve    <- pr.var/sum(pr.var)
 plot(pve, xlab = 'PCA', ylab = 'Variance explained', type = 'b')
